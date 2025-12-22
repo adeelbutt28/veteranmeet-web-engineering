bc86d64d0b74e144d88af94221bbb379590e0f8f
adeelbutt28 <adeelbuttdev@gmail.com>
2025-12-22 15:19:41 +0500

    Implement authentication APIs with JWT and password encryppt

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -12,6 +13,9 @@ connectDB();
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
 
 // Test route
 app.get("/", (req, res) => {
